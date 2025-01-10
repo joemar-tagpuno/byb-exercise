9cd514602d8c8d8605e958a8f878c6d047162885
Joemar <contact@joemartagpuno.info>
2025-01-10 14:32:24 +0800

    chore: fix linting issue

--- apps/api/src/modules/asset-manager/AssetManagerResolvers.ts
+++ apps/api/src/modules/asset-manager/AssetManagerResolvers.ts
@@ -13,14 +13,14 @@ export class AssetManagerResolvers {
   resolvers() {
     return {
       Query: {
-        customerSecureFileDownload: async (_, { fileId, userId }, context) => {
+        customerSecureFileDownload: async (_, { fileId, userId }, _context) => {
           // const user: UserIdentity = context.user
           const user: UserIdentity = {
             id: '913a6046-9959-4190-bd12-48cc6e8d8563',
             userRole: 'user',
             email: 'me@joemartagpuno.info',
             phone: '1234567890',
-          };
+          }
           const request: CustomerSecureFileDownloadRequest = { fileId, userId }
 
           const isAuthorized = await this._customerSecureFileDownload.canExecute(user, request)
@@ -30,12 +30,12 @@ export class AssetManagerResolvers {
 
           return await this._customerSecureFileDownload.execute(request)
         },
-        unauthorizedCustomerSecureFileDownload: async (_, { fileId, userId }, context) => {
+        unauthorizedCustomerSecureFileDownload: async (_, { fileId, userId }, _context) => {
           const user: UserIdentity = {
             id: 'unauthorized-user-id',
             userRole: 'unverified_user',
             email: 'unverified_user@email.com',
-          };
+          }
           const request: CustomerSecureFileDownloadRequest = { fileId, userId }
 
           const isAuthorized = await this._customerSecureFileDownload.canExecute(user, request)
@@ -44,7 +44,7 @@ export class AssetManagerResolvers {
           }
 
           return await this._customerSecureFileDownload.execute(request)
-        }
+        },
       },
     }
   }
